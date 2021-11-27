c585b5e3711d8bf6b26f6a943653d4e68ec11973
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2021-11-26 23:55:25 -0300

    add CORS

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const { dbConnection } = require('./database/config');
+const cors = require('cors');
 
 const app = express();
 
 // DB Connection
 dbConnection();
+
+// CORS
+app.use(cors());
 
 const port = process.env.PORT;
 
